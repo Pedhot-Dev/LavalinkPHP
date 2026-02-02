0cacc4600eace54465643b07e91f5d7f80766f0b
Pedhot-Dev <wanzgamerz2@gmail.com>
2026-02-02 17:06:03 +0700

    ...

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,8 +11,7 @@
 |
 */
 
-pest()->extend(Tests\TestCase::class)->in('Feature');
-pest()->extend(Tests\TestCase::class)->in('Unit');
+uses(Tests\TestCase::class)->in('Feature', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
